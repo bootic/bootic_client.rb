c2847421d5b651ded539e93ab77c552a7d936dcd
Ismael Celis <ismaelct@gmail.com>
2015-03-21 13:30:34 +0000

    BasicAuth strategy allows client to be used with Basic Auth APIs.

---
+++ lib/bootic_client/strategies/basic_auth.rb
@@ -0,0 +1,27 @@
+require 'bootic_client/strategies/strategy'
+
+module BooticClient
+  module Strategies
+    class BasicAuth < Strategy
+
+      def inspect
+        %(#<#{self.class.name} root: #{config.api_root} username: #{options[:username]}>)
+      end
+
+      protected
+
+      def validate!
+        raise ArgumentError, "options MUST include username" unless options[:username]
+        raise ArgumentError, "options MUST include password" unless options[:password]
+      end
+
+      def client
+        @client ||= Client.new(options) do |c|
+          c.request :basic_auth, options[:username], options[:password]
+        end
+      end
+    end
+  end
+
+  strategies[:basic_auth] = Strategies::BasicAuth
+end
---
+++ spec/basic_auth_strategy_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe 'BooticClient::Strategies::BasicAuth' do
+  require 'webmock/rspec'
+
+  let(:root_data) {
+    {
+      '_links' => {
+        'a_product' => {'href' => 'https://api.bootic.net/v1/products/1'}
+      },
+      'message' => "Hello!"
+    }
+  }
+
+  let(:product_data) {
+    {'title' => 'iPhone 6 Plus'}
+  }
+
+  let(:client) { BooticClient.client(:basic_auth, username: 'foo', password: 'bar') }
+
+  describe '#inspect' do
+    it 'is informative' do
+      expect(client.inspect).to eql %(#<BooticClient::Strategies::BasicAuth root: https://api.bootic.net/v1 username: foo>)
+    end
+  end
+  context 'with missing credentials' do
+    it 'raises error' do
+      expect{
+        BooticClient.client(:basic_auth)
+      }.to raise_error
+    end
+  end
+
+  context 'with invalid BasicAuth credentials' do
+    let!(:root_request) do
+      stub_request(:get, 'https://foo:bar@api.bootic.net/v1')
+        .to_return(status: 401, body: JSON.dump(root_data))
+    end
+
+    it 'raises an Unauthorized error' do
+      expect{ client.root }.to raise_error(BooticClient::UnauthorizedError)
+      expect(root_request).to have_been_requested
+    end
+  end
+
+  context 'with valid BasicAuth credentials' do
+    let!(:root_request) do
+      stub_request(:get, 'https://foo:bar@api.bootic.net/v1')
+        .to_return(status: 200, body: JSON.dump(root_data))
+    end
+
+    let!(:product_request) do
+      stub_request(:get, 'https://foo:bar@api.bootic.net/v1/products/1')
+        .to_return(status: 200, body: JSON.dump(product_data))
+    end
+
+    let!(:root) { client.root }
+
+    it 'includes Basic Auth credentials in request' do
+      expect(root_request).to have_been_requested
+      expect(root.message).to eql('Hello!')
+    end
+
+    it 'follows links as normal, including Basic Auth in every request' do
+      product = root.a_product
+      expect(product_request).to have_been_requested
+      expect(product.title).to eql 'iPhone 6 Plus'
+    end
+  end
+end
