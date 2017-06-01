ebc202df693b57685fefbcdba4c34ecba3273be8
Ismael Celis <ismaelct@gmail.com>
2017-06-01 14:45:25 +0100

    Make sure WhinyURI works with more complex syntax

--- spec/whiny_uri_spec.rb
+++ spec/whiny_uri_spec.rb
@@ -13,6 +13,13 @@ describe BooticClient::WhinyURI do
       }.to raise_error BooticClient::InvalidURLError
     end
 
+    it 'understand different path segment syntax' do
+      uri = described_class.new('http://www.host.com/shops{/id}/{?foo}')
+      expect{
+        uri.expand(foo: 1)
+      }.to raise_error BooticClient::InvalidURLError
+    end
+
     it 'expands if all path variables provided' do
       expect(uri.expand(id: 123))
         .to eql 'http://www.host.com/shops/123/'
@@ -24,9 +31,22 @@ describe BooticClient::WhinyURI do
       }.to raise_error BooticClient::InvalidURLError
     end
 
+    it 'understand variable lists in query' do
+      uri = described_class.new('http://www.host.com/shops{/id}/{?foo,bar}')
+      expect{
+        uri.expand(id: 123, foo: 'foo', bar: 'bar', nope: 'nope')
+      }.to raise_error BooticClient::InvalidURLError
+    end
+
     it 'expands if passing declared query variables' do
       expect(uri.expand(id: 123, foo: 'yes'))
         .to eql 'http://www.host.com/shops/123/?foo=yes'
+    end
+
+    it 'expands if passing declared query variables defined as list' do
+      uri = described_class.new('http://www.host.com/shops{/id}/{?foo,bar}')
+      expect(uri.expand(id: 123, foo: 'yes', bar: 'no'))
+        .to eql 'http://www.host.com/shops/123/?foo=yes&bar=no'
     end
   end
 end
